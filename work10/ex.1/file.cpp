902dbf8b393b5bce42d6d9d4876522f0b21c881e
txlxntless <alenaegorovna2007@gmail.com>
2026-04-15 22:05:44 +0700

    Create file.cpp

---
+++ work10/ex.1/file.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+// функция заполнения в зависимости от выбора пользователя
+void fillChoice(int choice, int n, int m, int** M) {
+    if (choice == 1) {
+            cout << "Введите элементы матрицы:" << endl;
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                    cin >> M[i][j];
+        } 
+        else if (choice == 2) {
+            string filename;
+            cout << "Введите имя файла: ";
+            cin >> filename;
+            ifstream fin(filename);
+            if (!fin) {
+                cout << "Ошибка: файл не найден! Введите матрицу с экрана:" << endl;
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < m; j++)
+                        cin >> M[i][j];
+            } else {
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < m; j++)
+                        fin >> M[i][j];
+                fin.close();
+            }
+        } 
+        else if (choice == 3) {
+            int A, B;
+            cout << "Введите интервал [a, b]: ";
+            cin >> A >> B;
+            RandomMatr(M, n, m, A, B);
+        }
+    }
+    // функция вывода в файл
+    void OutFileMatr(int** M, int n, int m) {
+        ofstream fout("output_matrix.txt");
+        if (fout.is_open()) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    fout << M[i][j] << " ";
+                }
+                fout << endl;
+            }
+            fout.close();
+            cout << "\nМатрица успешно сохранена в файл output_matrix.txt" << endl;
+        } else {
+            cout << "Ошибка при открытии файла!" << endl;
+        }
+}
